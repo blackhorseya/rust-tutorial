0b7c670e73eada2782c06ffebba7ea638faf5c2d
Sean Zheng <blackhorseya@gmail.com>
2023-03-04 09:07:42 +0800

    feat: update draw

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,8 +1,19 @@
 extern crate piston_window;
 extern crate rand;
 
+use piston_window::{PistonWindow, WindowSettings};
+
+use crate::draw::to_coord_u32;
+
 mod draw;
 mod snake;
 
 fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
 }
