8e298e3493175328083affe68f511559cab889cb
Sean Zheng <blackhorseya@gmail.com>
2023-03-13 09:36:11 +0800

    feat: impl chat client

--- chat_client/src/main.rs
+++ chat_client/src/main.rs
@@ -1,3 +1,64 @@
+use std::io::{ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc;
+use std::sync::mpsc::TryRecvError;
+use std::time::Duration;
+use std::{io, thread};
+
+const SERVER_ADDR: &str = "127.0.0.1:5001";
+const MSG_SIZE: usize = 32;
+
+fn sleep() {
+    thread::sleep(Duration::from_millis(100));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = TcpStream::connect(SERVER_ADDR).expect("Stream failed to connect");
+    client
+        .set_nonblocking(true)
+        .expect("failed to initialize non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                println!("message recv {:?}", msg);
+            }
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("connection with server was severed");
+                break;
+            }
+        }
+
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("writing to socket failed");
+                println!("message sent {:?}", msg);
+            }
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break,
+        }
+
+        sleep()
+    });
+
+    println!("Write a message:");
+    loop {
+        let mut buff = String::new();
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":q" || tx.send(msg).is_err() {
+            break;
+        }
+    }
+    println!("bye bye!")
 }
